8ce523111ab0ef65f414e1b72eb835c74be5596e
Leon Dail <leondailofficial@gmail.com>
2019-09-24 12:32:46 -0700

    added genre & country

--- server.js
+++ server.js
@@ -3,5 +3,49 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 
-let movies = require('movies-data-small.json');
+let movies = require('./movies-data-small.json');
 
+let countriesPossible = movies.map(movie => movie.country.toLowerCase());
+console.log(countriesPossible);
+let genresPossible = movies.map(movie => movie.genre.toLowerCase());
+console.log(genresPossible);
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+app.listen(8000, () => {
+  console.log('Listening!');
+});
+
+app.get('/movies', (req,res) => {
+  let newResults = movies;
+  let { genre, country, avg_vote } = req.query;
+
+  if (genre) {
+    let lower = genre.toLowerCase();
+    if(!genresPossible.includes(lower)) {
+      res.status(400).send('Must pick a matching genre');
+    }
+    newResults = movies.filter(movie => {
+      return movie['genre'].toLowerCase() === lower;
+    });
+  }
+  if (country) {
+    let lower = country.toLowerCase();
+    if(!countriesPossible.includes(lower)) {
+      res.status(400).send('Must pick a matching country');
+    }
+    newResults = movies.filter(movie => {
+      return movie['country'].toLowerCase() === lower;
+    });
+  }
+  
+
+
+
+
+  res.send(newResults);
+});
